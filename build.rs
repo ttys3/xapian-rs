f6ed7f88742fdc633aa93f4127efbdfb7195e33d
ttyS3 <ttys3.rust@gmail.com>
2023-06-10 21:42:39 +0800

    chore: support 1.4 and 1.5 header by feature

--- build.rs
+++ build.rs
@@ -2,6 +2,13 @@ use std::env;
 use std::path::Path;
 
 fn main() -> miette::Result<()> {
+    let xapian_15 = env::var("CARGO_FEATURE_XAPIAN_1_5").is_ok();
+    let pkg_config_lib_name = if xapian_15 {
+        "xapian-core-1.5"
+    } else {
+        "xapian-core"
+    };
+
     let mut vendored_xapian = env::var("CARGO_FEATURE_VENDORED_XAPIAN").is_ok();
     let try_to_use_system_xapian = !vendored_xapian;
 
@@ -10,11 +17,11 @@ fn main() -> miette::Result<()> {
         let mut cfg = pkg_config::Config::new();
         // /usr/lib64/pkgconfig/xapian-core.pc
         // pkg-config --modversion xapian-core
-        if let Ok(lib) = cfg.range_version("1.4.0".."1.5.0").probe("xapian-core") {
+        if let Ok(lib) = cfg.range_version("1.4.0".."1.5.99").probe(pkg_config_lib_name) {
             for include in &lib.include_paths {
                 println!("cargo:root={}", include.display());
             }
-            println!("cargo:info=Found system xapian");
+            println!("cargo:info=Found system xapian: {}", pkg_config_lib_name);
         } else {
             println!("cargo:warning=Failed to find system xapian, falling back to vendored");
             vendored_xapian = true;
@@ -25,10 +32,21 @@ fn main() -> miette::Result<()> {
     // xapian 1.5 /usr/include/xapian-1.5/xapian.h and /usr/include/xapian-1.5/xapian/*.h
     // xapian 1.4 /usr/include/xapian.h and /usr/include/xapian/*.h
     let manifest_dir = env::var_os("CARGO_MANIFEST_DIR").unwrap();
-    let xapian_include_dir = if vendored_xapian {
-        Path::new(&manifest_dir).join("xapian/xapian-core/include")
+
+    let sys_include_dir = if xapian_15 {
+        "/usr/include/xapian-1.5"
     } else {
-        Path::new(&manifest_dir).join("include")
+        "/usr/include"
+    };
+
+    let xapian_include_dir = if vendored_xapian {
+        if xapian_15 {
+            Path::new(&manifest_dir).join("include/xapian-1.5")
+        } else {
+            Path::new(&manifest_dir).join("include/xapian-1.4")
+        }
+    } else {
+        sys_include_dir
     };
 
     let src_path = Path::new(&manifest_dir).join("src");
