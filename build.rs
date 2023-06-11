c759fe1a32eb63904b1139d89b9b2e2ed9468fec
ttyS3 <ttys3.rust@gmail.com>
2023-06-11 12:22:25 +0800

    build: fix up build

--- build.rs
+++ build.rs
@@ -1,11 +1,14 @@
 use cxx_build::CFG;
 use std::env;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
-fn main() {
+fn main() -> miette::Result<()> {
     if cfg!(trybuild) {
-        return;
+        return Ok(());
     }
+    let xapian_15 = env::var("CARGO_FEATURE_XAPIAN_1_5").is_ok();
+    let pkg_config_lib_name = if xapian_15 { "xapian-core-1.5" } else { "xapian-core" };
+    let link_lib_name = if xapian_15 { "xapian-1.5" } else { "xapian" };
 
     let mut vendored_xapian = env::var("CARGO_FEATURE_VENDORED_XAPIAN").is_ok();
     let try_to_use_system_xapian = !vendored_xapian;
@@ -15,22 +18,37 @@ fn main() {
         let mut cfg = pkg_config::Config::new();
         // /usr/lib64/pkgconfig/xapian-core.pc
         // pkg-config --modversion xapian-core
-        if let Ok(lib) = cfg.range_version("1.4.0".."1.5.0").probe("xapian-core") {
+        if let Ok(lib) = cfg.range_version("1.4.0".."1.5.99").probe(pkg_config_lib_name) {
             for include in &lib.include_paths {
                 println!("cargo:root={}", include.display());
             }
-            println!("cargo:info=Found system xapian");
+            println!(
+                "cargo:warning=found and use system xapian: {}, include_paths: {:?}, libs: {:?}",
+                pkg_config_lib_name, &lib.include_paths, &lib.libs
+            );
         } else {
-            println!("cargo:warning=Failed to find system xapian, falling back to vendored");
+            println!("cargo:warning=failed to find system xapian, falling back to vendored");
             vendored_xapian = true;
         }
+    } else {
+        println!("cargo:warning=use vendored xapian");
     }
 
+    // include path
+    // xapian 1.5 /usr/include/xapian-1.5/xapian.h and /usr/include/xapian-1.5/xapian/*.h
+    // xapian 1.4 /usr/include/xapian.h and /usr/include/xapian/*.h
     let manifest_dir = env::var_os("CARGO_MANIFEST_DIR").unwrap();
+
+    let sys_include_dir = if xapian_15 { "/usr/include/xapian-1.5" } else { "/usr/include" };
+
     let xapian_include_dir = if vendored_xapian {
-        Path::new(&manifest_dir).join("xapian/xapian-core/include")
+        if xapian_15 {
+            Path::new(&manifest_dir).join("include/xapian-1.5")
+        } else {
+            Path::new(&manifest_dir).join("include/xapian-1.4")
+        }
     } else {
-        Path::new(&manifest_dir).join("include")
+        PathBuf::from(sys_include_dir)
     };
     CFG.exported_header_dirs.push(&xapian_include_dir);
 
@@ -51,16 +69,16 @@ fn main() {
     // static, dylib, framework, link-arg
     if vendored_xapian {
         println!("cargo:rustc-link-search=all=xapian/xapian-core/.libs");
-        println!("cargo:rustc-link-lib=static=xapian-1.5");
-        println!("cargo:rustc-link-lib=dylib=xapian-1.5");
+        println!("cargo:rustc-link-lib=static={}", link_lib_name);
+        println!("cargo:rustc-link-lib=dylib={}", link_lib_name);
         // export LD_LIBRARY_PATH=xapian/xapian-core/.libs
     } else {
-        println!("cargo:rustc-link-lib=xapian");
+        println!("cargo:rustc-link-lib={}", link_lib_name);
     }
-
-    println!("cargo:rustc-link-lib=m");
+    println!("cargo:warning=link lib name: {}", link_lib_name);
 
     println!("cargo:rerun-if-changed=xapian-bind.cc");
     println!("cargo:rerun-if-changed=xapian-bind.h");
     println!("cargo:rerun-if-changed=src/lib.rs");
+    Ok(())
 }
